59d261a38f8b12e55c40780aba45140ac53e2c99
Alisson Fernandes <alissonvinicios04@gmail.com>
2024-10-12 14:05:49 -0300

    [DEV] increased style to 100%

--- src/VersionedModel.php
+++ src/VersionedModel.php
@@ -2,14 +2,14 @@
 
 namespace Kiqstyle\EloquentVersionable;
 
-use \Illuminate\Database\Eloquent\Model;
-use \Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class VersionedModel extends Model implements VersionableContract
 {
     use SoftDeletes, Versionable;
 
-    const NEXT_COLUMN = "next";
+    const NEXT_COLUMN = 'next';
 
     const VERSIONED_TABLE = null;
 
